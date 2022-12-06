c11e8e5e11153f98756f6a0e85f8cf8c2a9a568d
Jasper Anelechukwu <Jasper.Anelechukwu@infobip.com>
2022-12-06 05:30:51 +0100

    Fixed fetch events feature.

--- src/main/java/com/jasper/schoolbell/repositories/EventsRepository.java
+++ src/main/java/com/jasper/schoolbell/repositories/EventsRepository.java
@@ -2,10 +2,12 @@ package com.jasper.schoolbell.repositories;
 
 import com.jasper.schoolbell.Configuration;
 import com.jasper.schoolbell.entities.Event;
+import com.jasper.schoolbell.entities.EventOccurrence;
 import com.jasper.schoolbell.entities.Participant;
 
 import javax.inject.Inject;
 import javax.transaction.Transactional;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -27,6 +29,14 @@ public class EventsRepository {
                 configuration.getEntityManager()
                     .createQuery("SELECT p FROM Participant p WHERE p.event.id = ?1 AND p.host = true", Participant.class)
                     .setParameter(1, event.getId())
+                    .getResultList()
+            ))
+            .peek(event -> event.setEventOccurrences(
+                configuration.getEntityManager()
+                    .createQuery("SELECT eo FROM EventOccurrence eo WHERE eo.event.id = ?1 AND eo.startedAt >= ?2 ORDER BY eo.startedAt ASC", EventOccurrence.class)
+                    .setParameter(1, event.getId())
+                    .setParameter(2, LocalDateTime.now())
+                    .setMaxResults(1)
                     .getResultList()
             )).
             collect(Collectors.toList());
